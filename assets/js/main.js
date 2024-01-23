7939b76c9f3d4da2dcc5c18faaa680f6c9c9319e
Guilherme-Daneliv23 <gui.professional.profile@gmail.com>
2024-01-22 22:56:14 -0300

    criando loop no js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
-const quantidade = document.querySelectorAll(".quantidade")
+const quantidade = document.querySelectorAll(".quantidade");
 const enviar = document.getElementById("enviar");
 
 enviar.addEventListener('click', () => {
-    console.log(quantidade[1].value);
+    for(let i = 0; i < quantidade.length; i++)  {
+        console.log(quantidade[i].value);
+    }
+    
 })
