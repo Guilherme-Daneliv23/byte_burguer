f60a059f0614ff8356d3a4e84dfec136b741484e
Guilherme-Daneliv23 <gui.professional.profile@gmail.com>
2024-01-23 23:45:56 -0300

    terminando funcionalidades js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,27 +4,45 @@ const retorno = document.getElementById("retorno");
 
 const comanda = document.getElementById("comanda");
 
+const quantidadeComanda = document.querySelectorAll("#quantidade-comander");
+const valorComanda = document.querySelectorAll("#valor-comander");
+
+const valorPagamento = document.getElementById("valor-pagamento");
+
+console.log(quantidadeComanda);
+
 const precos = new Array (
     42.50,
     38.25,
     39.99,
     44.50
 )
-// enviar.addEventListener('click', () => {
-
-//     var soma = 0;
-
-//     for(let i = 0; i < quantidade.length; i++)  {
-
-//         soma += quantidade[i].value * precos[i];
-
-//     }
-
-//     retorno.value = soma;     
-    
-// })
 
 enviar.addEventListener('click', () =>   {
     comanda.classList.add("seletor");
+
+    exibePedido();
+    calculaPagamento();
 })
+
+function exibePedido()  {
+    for(let i = 0; i < quantidade.length; i++)  {
+        quantidadeComanda[i].innerHTML = "Quantidade: " + quantidade[i].value;
+        valorComanda[i].innerHTML = "Valor: " + quantidade[i].value * precos[i];
+
+    }
+}
+
+function calculaPagamento() {
+    
+    var soma = 0;
+
+    for(let i = 0; i < quantidade.length; i++)  {
+
+        soma += quantidade[i].value * precos[i];
+
+    }
+
+    valorPagamento.innerHTML = soma;
+}
 
